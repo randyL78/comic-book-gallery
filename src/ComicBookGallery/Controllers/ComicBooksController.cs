c1fadfbb97fd3803c0a56f03a06dc4c3f4df0145
RandyL <30959856+randyL78@users.noreply.github.com>
2018-11-09 21:45:00 -0500

    Update comic book detail view to be strongly typed

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace ComicBookGallery.Controllers
 {
@@ -11,19 +12,25 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = 
+                    "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artists[]
+                {
+                    new Artists() {Name = "Dan Slott", Role = "Script"},
+                    new Artists() {Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artists() {Name = "Victor Olazaba", Role = "Inks"},
+                    new Artists() {Name = "Edgar Delgado", Role = "Colors"},
+                    new Artists() {Name = "Chris Eliopoulos", Role = "Letters"},
+
+                }
+
             };
 
-            return View();
+            return View(comicBook);
 
         }
     }
